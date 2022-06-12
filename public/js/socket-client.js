1313031371f5c4af674bd6e8f051b37e620f308c
albertost5 <sancheztorresalberto@gmail.com>
2022-06-12 20:02:20 +0200

    Added socket.io and configured it to display messages from the server and client

---
+++ public/js/socket-client.js
@@ -0,0 +1,16 @@
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected..');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected..');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+})
---
+++ src/api/models/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+class Server {
+    
+    constructor( routes = [] ) {
+        this.routes = routes;
+        this.app = express();
+        this.port = process.env.PORT || 3001;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+
+
+        // Middlewares
+        this.middlewares();
+        
+        // Initialize controllers
+        this.initializeControllers();
+
+        // Sockets config
+        this.sockets();
+    }
+
+    initializeControllers() {
+        // this.routes.forEach( routes => {
+        //     this.app.use( '/',  routes.registerRoutes() );
+        // });
+
+        console.log(`Registered ${ this.routes.length } routes successfully!`);
+    }
+
+    middlewares() {
+        this.app.use( cors() );
+        this.app.use( express.static('public') );
+    }
+    
+    sockets() {
+        this.io.on('connection', socket => { 
+            console.log(`Client ${ socket.id } connected..`);
+
+            socket.on('disconnect', () => {
+                console.log(`Client ${ socket.id } disconnected.. `);
+            });
+        });
+    }
+
+    listen() {
+        this.server.listen( this.port, () => {
+            console.log(`Basic-web-socket listening on port ${ this.port }...`)
+        });
+    }
+}
+
+
+module.exports = Server;
